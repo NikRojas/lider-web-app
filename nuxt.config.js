a9c24caf8835c3efae684af143bbe8d1c31e2b39
Anthony <anthto527@gmail.com>
2023-06-14 17:54:16 -0500

    Agregado tiktok icon a footer

--- nuxt.config.js
+++ nuxt.config.js
@@ -306,9 +306,9 @@ export default {
   },
   proxy: {
     '/api/': {
-      target: 'https://admin.lider.com.pe/api/',
+      //target: 'https://admin.lider.com.pe/api/',
       //target: 'https://lider-admin.playgroup.pe/api/',
-      //target: 'http://127.0.0.1:8000/api/',
+      target: 'http://127.0.0.1:8000/api/',
       pathRewrite: { '^/api/': '' },
     }
   },
